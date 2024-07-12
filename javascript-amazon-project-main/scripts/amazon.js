9a3b6e66d3e119d30e03ebaf1f0cee1608597216
arunmeti2018 <arunameti2018@gmail.com>
2024-07-12 18:55:30 +0530

    data loaded from products.js and removed html content and added content from amazon.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,33 +1,4 @@
-const products=[
-    {
-    image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name :'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    ratings :{
-      stars:4.5,
-     count:87,
-    },
-    priceCents:1090 
-    },
-    {
-       image :'images/products/intermediate-composite-basketball.jpg',
-       name: 'Intermediate Size Basketball',
-       ratings:{
-        stars : 4,
-        count :127
-       },
-       priceCents:2095
-    },
-    {
-        image :'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-        ratings:{
-            stars:4.5,
-            count:56
-        },
-        priceCents:799
-    }
 
-];
  let productsHTML='';
 products.forEach((product)=>{
    productsHTML+=`<div class="product-container">
@@ -42,9 +13,9 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.ratings.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${product.ratings.count}
+              ${product.rating.count}
             </div>
           </div>
 
