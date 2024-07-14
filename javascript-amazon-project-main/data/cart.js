1f27285b8296d2ebd22fddcad0b6304773aaa58b
arunmeti2018 <arunameti2018@gmail.com>
2024-07-14 11:27:17 +0530

    saved cart items on localstorage using function savetostorage() and loading cart data from local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,6 @@
- export let cart=[
+ export let cart=JSON.parse(localStorage.getItem('cart'));
+ if(!cart){
+cart= [
   {
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity : 1
@@ -8,6 +10,11 @@
     quantity : 1
 }
  ];
+ }
+
+ function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+ }
  export function addToCart(productId){
     let matchingItem;
     cart.forEach((cartItem)=>{
@@ -23,6 +30,7 @@
         quantity : 1
       })
     }
+    saveToStorage();
   }
 
  export function deleteFromCart(productId){
@@ -33,6 +41,6 @@
       }
     })
     cart=newCart;
-   
+   saveToStorage();
   }
   
