29ec350cfa61fd97cbcd0c57d29f8258dd5b8193
arunmeti2018 <arunameti2018@gmail.com>
2024-07-16 00:53:21 +0530

    generated header html from headersummary.js

--- javascript-amazon-project-main/scripts/checkout/headerSummary.js
+++ javascript-amazon-project-main/scripts/checkout/headerSummary.js
@@ -3,10 +3,25 @@ export function renderHeaderSummary() {
   let cartQuantity = getCartQuantity();
 
 
-    let html = `
-        Checkout (<a class="return-to-home-link" href="amazon.html">${cartQuantity} items</a>)
-      `;
-    document.querySelector('.js-header-summary')
-      .innerHTML = html;
+  let html = `
+   <div class="header-content">
+      <div class="checkout-header-left-section">
+        <a href="amazon.html">
+          <img class="amazon-logo" src="images/amazon-logo.png">
+          <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+        </a>
+      </div>
+
+      <div class="checkout-header-middle-section  ">
+      Checkout (<a class="return-to-home-link" href="amazon.html">${cartQuantity} items</a>)
+      </div>
+
+      <div class="checkout-header-right-section">
+        <img src="images/icons/checkout-lock-icon.png">
+      </div>
+    </div> 
+   `;
+  document.querySelector('.js-header-summary')
+    .innerHTML = html;
 
 }
