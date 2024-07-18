ca39881d1d0da2719fb94909e7300d8df33da331
arunmeti2018 <arunameti2018@gmail.com>
2024-07-18 21:22:22 +0530

    final changes made commented loadproducts loading products from products.js

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,9 +1,9 @@
 import { renderOrderSummary } from './checkout/orderSummary.js'
 import { renderpaymnetSummary } from './checkout/paymnetSummary.js';
 import { renderHeaderSummary } from './checkout/headerSummary.js';
-import { products, loadProducts } from '../data/products.js'
-loadProducts(() => {
-  renderOrderSummary();
-  renderpaymnetSummary();
-  renderHeaderSummary();
-})
+
+
+renderOrderSummary();
+renderpaymnetSummary();
+renderHeaderSummary();
+
